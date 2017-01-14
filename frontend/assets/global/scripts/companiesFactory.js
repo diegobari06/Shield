c31d5fc7c4c5b29920df5ed3d39b9e78d08942ae
diegobari06 <diegobari06@hotmail.com>
2017-01-13 19:37:35 -0600

    fixinf

--- frontend/assets/global/scripts/companiesFactory.js
+++ frontend/assets/global/scripts/companiesFactory.js
@@ -1,19 +1,19 @@
-app.factory('companiesFunctions', function($http){
-  server = "http://localhost:3000/";
-  return {
-    get: function(id){
-      return $http.get(server+'companies/'+id+'.json');
-    },
-    getCompaniesList: function (){
-      return $http.get(server+'companies.json');
+app.factory('companiesFunctions', function($http) {
+    server = "http://localhost:3000/api/";
+    return {
+        get: function(id) {
+            return $http.get(server + 'companies/' + id + '.json');
+        },
+        getCompaniesList: function() {
+            return $http.get(server + 'companies.json');
+        }
     }
-  }
 });
 
-app.filter('intersect', function(){
-    return function(arr1, arr2){
+app.filter('intersect', function() {
+    return function(arr1, arr2) {
         return arr1.filter(function(n) {
-                   return arr2.indexOf(n) != -1
-               });
+            return arr2.indexOf(n) != -1
+        });
     };
 });
